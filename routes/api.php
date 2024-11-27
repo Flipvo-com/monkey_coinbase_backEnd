fa5d289dabfd75174e14130a58a71ac8ddef5f3a
Chris Nowlan <chrisnowlan321@gmail.com>
2024-11-27 04:56:42 -0500

    Adding Investments tables, etc

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\InvestmentController;
+use App\Http\Controllers\InvestmentTransactionController;
 use App\Http\Controllers\TraderController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrderController;
@@ -25,11 +27,38 @@ Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
 Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
 
 
+// todo - Youcef, what is this? I wish this didn't have student, teacher, parent, etc stuff
 Route::middleware('auth.multiGuard:student,teacher,parent,admin')->group(function () {
     Route::get('/state', [TraderController::class, 'getState']);
-
     Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
     Route::post('/getAccountOrders', [OrderController::class, 'getAccountOrders']);
     Route::post('/longPolling', [LongPollingController::class, 'getDataLongPolling']);
+});
 
+
+// Group routes for investment-related operations
+Route::prefix('investments')->group(function () {
+    // Fetch all investments
+    Route::get('/', [InvestmentController::class, 'index']);
+
+    // Create a new investment
+    Route::post('/', [InvestmentController::class, 'store']);
+
+    // Update an existing investment
+    Route::put('/{id}', [InvestmentController::class, 'update']);
+
+    // Delete an investment
+    Route::delete('/{id}', [InvestmentController::class, 'destroy']);
+});
+
+// Group routes for investment transactions
+Route::prefix('investment-transactions')->group(function () {
+    // Fetch all transactions
+    Route::get('/', [InvestmentTransactionController::class, 'index']);
+
+    // Create a new transaction
+    Route::post('/', [InvestmentTransactionController::class, 'store']);
+
+    // Fetch transactions for a specific user
+    Route::get('/user/{userId}', [InvestmentTransactionController::class, 'getByUser']);
 });
